06483551b57f635be311aaff444f244f83087d6f
abrarasif11 <faasif929@gmail.com>
2022-11-24 11:35:26 +0600

    mong0-connect

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +14,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Goriber Seller Site Server is Running')
 })
+
+//Mongo DB connection //
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vhdpi0m.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.listen(port, () => {
     console.log(`Goriber Seller Server Running on ${port}`);
